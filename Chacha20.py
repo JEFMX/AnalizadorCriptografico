8d159fac523b8a8e1fd665c1fcd13e24dfb9d3a9
Jesús Eduardo Flores González <57874646+JEFMX@users.noreply.github.com>
2022-04-14 15:42:58 -0500

    ChaCha20 v2.0

--- Chacha20.py
+++ Chacha20.py
@@ -1,28 +1,61 @@
 from base64 import b64encode
 from base64 import b64decode
-from webbrowser import get
+from itertools import tee
+from pickletools import read_uint1
+import time
 from Crypto.Cipher import ChaCha20
 from Crypto.Random import get_random_bytes
 
-#Implementacion de cifrado y descifrado con CHACHA20 usando PyCryptodome
-TextoEnClaro = b'Attack at dawn'
+def mide_tiempo(funcion):
+    def funcion_medida(*args, **kwargs):
+        inicio = time.time()
+        c = funcion(*args, **kwargs)
+        tiempo =(time.time() - inicio)-1
+        print(tiempo) #Para ver el tiempo en consola es temporal
+        archivo = open("TiempoChaCha.txt", 'a')
+        archivo.write(str(tiempo)+ '\n')
+        return c
+    return funcion_medida
 
-#Se genera la llave y el nonce entradas para el algoritmo 
-key = get_random_bytes(32) #Esto equivale a 256 bits = 32 bytes
-nonce = get_random_bytes(12) #Se genera el nonce esto deacuerdo al RFC7539
-#Se le indica al algoritmo las entradas
-Cifrado = ChaCha20.new(key=key, nonce=nonce)
-#Inicia el cifrado
-TextoCifrado = Cifrado.encrypt(TextoEnClaro)
-ct = b64encode(TextoCifrado).decode('utf-8') #Es importante ya que permite hacer el descifrado lo pasa a base64
-resultCifrado = {'nonce':nonce, 'TextoCifrado':ct}
-print(resultCifrado)
+def generar_key_nonce_entrada():
+    #Implementacion de cifrado y descifrado con CHACHA20 usando PyCryptodome
+    TextoEnClaro = b'Attack at dawn'
 
-#Descifrar
-TextoADescifrar = b64decode(resultCifrado['TextoCifrado'])
-#Se le indica al algoritmo las entradas
-Descifrado = ChaCha20.new(key=key, nonce=nonce)
-#Inicia el Descifrado
-TextoDescifrado = Descifrado.decrypt(TextoADescifrar)
-resultDescifrado = {'nonce':nonce, 'TextoDescifrado':TextoDescifrado}
-print(resultDescifrado)
+    #Se genera la llave y el nonce entradas para el algoritmo 
+    key = get_random_bytes(32) #Esto equivale a 256 bits = 32 bytes
+    nonce = get_random_bytes(12) #Se genera el nonce esto deacuerdo al RFC7539
+    #Se le indica al algoritmo las entradas
+    return key, nonce, TextoEnClaro
+
+@mide_tiempo
+def cifrado(key, nonce,TextoEnClaro):  
+    Cifrado = ChaCha20.new(key=key, nonce=nonce)
+    TextoCifrado = Cifrado.encrypt(TextoEnClaro)
+    time.sleep(1) #es necesario apra calcular el tiempo
+    return TextoCifrado
+
+def CifradoChacha20(key,nonce,TextoEnClaro): 
+    TextoCifrado = cifrado(key, nonce, TextoEnClaro)
+    ct = b64encode(TextoCifrado).decode('utf-8') #Es importante ya que permite hacer el descifrado lo pasa a base64
+    resultCifrado = {'nonce': nonce, 'TextoCifrado':ct}
+    print(resultCifrado)
+    return resultCifrado
+
+def ChaCha20_algoritmo():
+    key,nonce,TextoEnClaro = generar_key_nonce_entrada()
+    temp = CifradoChacha20(key,nonce,TextoEnClaro)
+    decifrar(temp, key, nonce)
+    
+@mide_tiempo
+def decifrar(resultCifrado,key,nonce):
+    #Descifrar
+    TextoADescifrar = b64decode(resultCifrado['TextoCifrado'])
+    #Se le indica al algoritmo las entradas
+    Descifrado = ChaCha20.new(key=key, nonce=nonce)
+    #Inicia el Descifrado
+    TextoDescifrado = Descifrado.decrypt(TextoADescifrar)
+    time.sleep(1) #es necesario apra calcular el tiemp 
+    resultDescifrado = {'nonce':nonce, 'TextoDescifrado':TextoDescifrado}
+    print(resultDescifrado)
+
+ChaCha20_algoritmo()
