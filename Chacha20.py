f9c9a41e3bd378d9d1eb2bf9ee6ce3d86c434261
Jesús Eduardo Flores González <57874646+JEFMX@users.noreply.github.com>
2022-04-14 16:50:29 -0500

    Main v1.0 y Chacha20 v2.1

--- Chacha20.py
+++ Chacha20.py
@@ -1,7 +1,5 @@
 from base64 import b64encode
 from base64 import b64decode
-from itertools import tee
-from pickletools import read_uint1
 import time
 from Crypto.Cipher import ChaCha20
 from Crypto.Random import get_random_bytes
@@ -10,17 +8,16 @@ def mide_tiempo(funcion):
     def funcion_medida(*args, **kwargs):
         inicio = time.time()
         c = funcion(*args, **kwargs)
-        tiempo =(time.time() - inicio)-1
-        print(tiempo) #Para ver el tiempo en consola es temporal
+        tiempo =(time.time() - inicio)-0.0001
+        #print(tiempo) #Para ver el tiempo en consola es temporal
         archivo = open("TiempoChaCha.txt", 'a')
         archivo.write(str(tiempo)+ '\n')
         return c
     return funcion_medida
 
-def generar_key_nonce_entrada():
+def generar_key_nonce_entrada(vector_prueba):
     #Implementacion de cifrado y descifrado con CHACHA20 usando PyCryptodome
-    TextoEnClaro = b'Attack at dawn'
-
+    TextoEnClaro = bytes(vector_prueba, 'utf-8')
     #Se genera la llave y el nonce entradas para el algoritmo 
     key = get_random_bytes(32) #Esto equivale a 256 bits = 32 bytes
     nonce = get_random_bytes(12) #Se genera el nonce esto deacuerdo al RFC7539
@@ -31,18 +28,18 @@ def generar_key_nonce_entrada():
 def cifrado(key, nonce,TextoEnClaro):  
     Cifrado = ChaCha20.new(key=key, nonce=nonce)
     TextoCifrado = Cifrado.encrypt(TextoEnClaro)
-    time.sleep(1) #es necesario apra calcular el tiempo
+    time.sleep(0.0001) #es necesario apra calcular el tiempo
     return TextoCifrado
 
 def CifradoChacha20(key,nonce,TextoEnClaro): 
     TextoCifrado = cifrado(key, nonce, TextoEnClaro)
     ct = b64encode(TextoCifrado).decode('utf-8') #Es importante ya que permite hacer el descifrado lo pasa a base64
     resultCifrado = {'nonce': nonce, 'TextoCifrado':ct}
-    print(resultCifrado)
+    #print(resultCifrado)
     return resultCifrado
 
-def ChaCha20_algoritmo():
-    key,nonce,TextoEnClaro = generar_key_nonce_entrada()
+def ChaCha20_algoritmo(vector_prueba):
+    key,nonce,TextoEnClaro = generar_key_nonce_entrada(vector_prueba)
     temp = CifradoChacha20(key,nonce,TextoEnClaro)
     decifrar(temp, key, nonce)
     
@@ -54,8 +51,6 @@ def decifrar(resultCifrado,key,nonce):
     Descifrado = ChaCha20.new(key=key, nonce=nonce)
     #Inicia el Descifrado
     TextoDescifrado = Descifrado.decrypt(TextoADescifrar)
-    time.sleep(1) #es necesario apra calcular el tiemp 
+    time.sleep(0.0001) #es necesario apra calcular el tiemp 
     resultDescifrado = {'nonce':nonce, 'TextoDescifrado':TextoDescifrado}
-    print(resultDescifrado)
-
-ChaCha20_algoritmo()
+    #print(resultDescifrado)
---
+++ main.py
@@ -0,0 +1,21 @@
+from hashlib import sha1, sha224
+from Chacha20 import ChaCha20_algoritmo
+import random
+import string
+def generacion_vector_prueba():
+    for i in range(100):
+        longitud_vector = random.randint(10,300)
+        vector_prueba = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(longitud_vector))
+        archivo = open("vectores.txt", 'a')
+        archivo.write(vector_prueba + '\n')
+
+def main():
+    generacion_vector_prueba()
+    archivo = open("vectores.txt", 'r')
+    linea = archivo.readlines()
+    p = 0
+    for i in linea: 
+        p += 1
+        print("Porcentaje: {}%".format(p))
+        ChaCha20_algoritmo(i.strip('\n'))
+main()
