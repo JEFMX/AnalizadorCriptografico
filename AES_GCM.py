ab01ad5793b6b2244c94d2eb975e6b90d4f135f9
Jesús Eduardo Flores González <57874646+JEFMX@users.noreply.github.com>
2022-04-15 17:33:04 -0500

    GCM con falla

--- AES_GCM.py
+++ AES_GCM.py
@@ -1,3 +1,26 @@
 import time
 from Crypto.Cipher import AES
-from MedidorTiempo import mide_tiempo
+from MedidorTiempo import mide_tiempo_AES_GCM
+from Crypto.Util.Padding import pad, unpad
+
+@mide_tiempo_AES_GCM
+def cifrado(key, text):
+    cipher = AES.new(key, AES.MODE_GCM)
+    cipherText = cipher.encrypt(pad(text, 32))
+    time.sleep(0.0001)  # es necesario apra calcular el tiempo
+    return cipherText
+
+
+#@mide_tiempo_AES_GCM
+def descifrado(key, cipherText):
+    decipher = AES.new(key, AES.MODE_GCM)
+    plainText = unpad(decipher.decrypt(cipherText), 32)
+    time.sleep(0.0001)  # es necesario apra calcular el tiempo
+    return plainText
+
+
+def AES_GCM_algoritmo(key, text):
+    text = bytes(text, "utf-8")
+    cipherText = cifrado(key, text)
+    plainText = descifrado(key, cipherText)
+    #print(str(text) + "\n" + str(cipherText) + "\n" + str(plainText))
