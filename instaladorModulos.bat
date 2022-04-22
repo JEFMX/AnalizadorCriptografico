8c1be8ad1521d3580aa8b4f1f220bf5ba8a738e6
Jesús Eduardo Flores González <57874646+JEFMX@users.noreply.github.com>
2022-04-21 19:02:57 -0500

    Instalador módulos Windows

---
+++ instaladorModulos.bat
@@ -0,0 +1,10 @@
+REM Este archivo se encarga de instalar lo modulos de python para el Analizador Criptografico
+@echo on
+REM Instalacion del modulo python que contien el set de algoritmos
+pip install pycryptodome
+timeout 3
+REM Instalacion del modulo python que contien el set de algoritmos
+pip install matplotlib
+timeout 3
+echo Complete
+timeout 5
